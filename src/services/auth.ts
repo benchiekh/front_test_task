135ec5f0e7e1fce3211ec81562d5074d20fd149b
Iheb Ben Chiekh Ahmed <iheb.benchiekhahmed@gmail.com>
2025-08-16 16:41:25 +0200

    corriger erru dep

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,21 +1,15 @@
 import { api } from "./config";
 
 export async function login(identifier: string, password: string) {
-  try {
+ 
     const res = await api.post("/auth/local", { identifier, password });
     return res.data; // jwt + user
-  } catch (err: any) {
-    console.error("❌ Login error:", err.response?.data || err.message);
-    return { error: err.response?.data };
-  }
+  
 }
 
 export async function register(username: string, email: string, password: string) {
-  try {
+ 
     const res = await api.post("/auth/local/register", { username, email, password });
     return res.data; // jwt + user
-  } catch (err: any) {
-    console.error("❌ Register error:", err.response?.data || err.message);
-    return { error: err.response?.data };
-  }
+  
 }
